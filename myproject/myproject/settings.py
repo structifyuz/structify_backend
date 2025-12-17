22cce9f405cb03efe0c0a823f9e1c8c71d224ef8
Aziz Ulmasov <azizulmasov93@gmail.com>
2025-12-18 01:05:45 +0500

    remove .env from git

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -123,9 +123,11 @@ USE_I18N = True
 USE_TZ = True
 
 # --- STATIC & MEDIA ---
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static'] 
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles"
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
